dcb6710726dd24b6584f61a52ff2cdc1532d5596
Piumi Lokuge <piumi1993indeewari@gmail.com>
2017-12-09 11:13:46 +0530

    logout

--- Front-end/src/UI/ATE/AddLoco/LocoTabController.java
+++ Front-end/src/UI/ATE/AddLoco/LocoTabController.java
@@ -2,8 +2,6 @@ package UI.ATE.AddLoco;
 
 //import Model.FailureOccured;
 //import UI.Index.Connect;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,17 +10,7 @@ import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.ResourceBundle;
-
-import static java.util.Arrays.asList;
 
 
 /**
@@ -36,7 +24,7 @@ public class LocoTabController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            AnchorPane locoBase = FXMLLoader.load(getClass().getResource("locoView.fxml"));
+            AnchorPane locoBase = FXMLLoader.load(getClass().getResource("../../Dashboard/emptyView.fxml"));
             UI.Index.IndexController.setNodeLoco(locoBase);
             viewLocoTab.setContent(locoBase);
         } catch (IOException e) {
